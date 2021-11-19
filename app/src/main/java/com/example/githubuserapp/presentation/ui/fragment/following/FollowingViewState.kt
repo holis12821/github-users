46509d5b13ef70fbc0638775804ec116eb0f4577
Nurholis <nur.kholis@ottencoffee.co.id>
2021-11-20 00:11:45 +0700

    Optimize Import

--- app/src/main/java/com/example/githubuserapp/presentation/ui/fragment/following/FollowingViewState.kt
+++ app/src/main/java/com/example/githubuserapp/presentation/ui/fragment/following/FollowingViewState.kt
@@ -1,11 +1,10 @@
 package com.example.githubuserapp.presentation.ui.fragment.following
 
 import com.example.githubuserapp.data.response.model.ItemsItem
-import com.example.githubuserapp.presentation.ui.fragment.followers.FollowersViewState
 
 sealed class FollowingViewState {
-    object Init: FollowingViewState()
-    data class Progress(val isLoading: Boolean): FollowingViewState()
-    data class ShowMessage(val message: String): FollowingViewState()
-    data class ShowFollowing(val list: List<ItemsItem>?): FollowingViewState()
+    object Init : FollowingViewState()
+    data class Progress(val isLoading: Boolean) : FollowingViewState()
+    data class ShowMessage(val message: String) : FollowingViewState()
+    data class ShowFollowing(val list: List<ItemsItem>?) : FollowingViewState()
 }
